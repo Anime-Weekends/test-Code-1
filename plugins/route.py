f17af49743d8128a3ff114d47c399e5061bb543c
Jҽϝϝɾҽყ ʂαɱα <jeffreymosesdj@gmail.com>
2024-12-12 19:01:09 +0530

    Update route.py

--- plugins/route.py
+++ plugins/route.py
@@ -5,4 +5,4 @@ routes = web.RouteTableDef()
 
 @routes.get("/", allow_head=True)
 async def root_route_handler(request):
-    return web.json_response("Advance_Bot_Light")
+    return web.json_response("Weekends Premium Bot")
