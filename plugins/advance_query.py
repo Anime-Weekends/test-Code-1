ad3133c5f51a3a86e70e01dfd8dd7b65214dbb50
rohit3607 <rohitplayer87089@gmail.com>
2024-12-24 10:37:10 +0530

    Update advance_query.py

--- plugins/advance_query.py
+++ plugins/advance_query.py
@@ -572,7 +572,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
         except Exception as e:
             print(f"! Error Occured on callback data = 'more_settings' : {e}")
             
-
+    
     elif data == 'req_fsub':
         #if await authoUser(query, query.from_user.id, owner_only=True) :
         await query.answer("♻️ Qᴜᴇʀʏ Pʀᴏᴄᴇssɪɴɢ....")
@@ -595,7 +595,21 @@ async def cb_handler(client: Bot, query: CallbackQuery):
         except Exception as e:
             print(f"! Error Occured on callback data = 'chng_req' : {e}")
         
-            
+    elif data == "buy_prem":
+         await query.message.edit_text(
+            text=f"👋 {query.from_user.username}\n\n🎖️ Available Plans :\n\n● {PRICE1} rs For 7 Days Prime Membership\n\n● {PRICE2} rs For 1 Month Prime Membership\n\n● {PRICE3} rs For 3 Months Prime Membership\n\n● {PRICE4} rs For 6 Months Prime Membership\n\n● {PRICE5} rs For 1 Year Prime Membership\n\n\n💵 UPI ID -  <code>{UPI_ID}</code>\n\n\n📸 QR - ᴄʟɪᴄᴋ ʜᴇʀᴇ ᴛᴏ ꜱᴄᴀɴ ({UPI_IMAGE_URL})\n\n♻️ If payment is not getting sent on above given QR code then inform admin, he will give you new QR code\n\n\n‼️ Must Send Screenshot after payment",
+            disable_web_page_preview=True,
+            reply_markup = InlineKeyboardMarkup(
+                [   
+                    [
+                        InlineKeyboardButton("Send Payment Screenshot(ADMIN) 📸", url=(SCREENSHOT_URL))
+                    ],
+                    [
+                        InlineKeyboardButton("🔒 Close", callback_data = "close")
+                    ]
+                ]
+            )
+            )
                 
                     
                  
