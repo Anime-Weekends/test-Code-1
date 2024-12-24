f64c26fa7da937e23237d511b501e9d4eb4dc2c4
rohit3607 <rohitplayer87089@gmail.com>
2024-12-24 10:42:15 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -168,12 +168,11 @@ async def start_command(client: Client, message: Message):
                 mention=message.from_user.mention,
                 id=message.from_user.id
             ),
-            reply_markup=reply_markup
+            reply_markup = reply_markup,
+	        message_effect_id=5104841245755180586 #🔥
         )
-        try:
-            await message.delete()
-        except:
-            pass
+        try: await message.delete()
+        except: pass
 
    
 ##===================================================================================================================##
