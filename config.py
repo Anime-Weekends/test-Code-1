db17007ff38a05aa51466da47545bc631157c1d4
Jҽϝϝɾҽყ ʂαɱα <jeffreymosesdj@gmail.com>
2024-12-30 16:44:05 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ DB_NAME = os.environ.get("DATABASE_NAME", "jeffy25rf")
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #Collection of pics for Bot // #Optional but atleast one pic link should be replaced if you don't want predefined links
-PICS = (os.environ.get("PICS", "https://envs.sh/Uc0.jpg https://envs.sh/UkA.jpg https://envs.sh/Uk_.jpg https://envs.sh/Ukc.jpg https://envs.sh/UkZ.jpg https://envs.sh/UkK.jpg")).split() #Required
+PICS = (os.environ.get("PICS", "")).split() #Required
 
 #set your Custom Caption here, Keep None for Disable Custom Caption
 CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION", None)
